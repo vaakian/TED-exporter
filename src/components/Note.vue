f1a311d91184dfead43593a4149118703c62f742
vaakian <vaakian@gmail.com>
2022-05-25 22:39:01 +0800

    tips & tricks

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="mx-auto w-90% max-w-388px border p-4" text="blue left">
     <p text-red fw-bold>⚠️Caution:</p>
-    <p>
+    <p mb-2>
       1) 有<span text-cyan fw-bold>极少部分</span
-      >获取到的双语演讲稿无法完全对应，
+      >获取到的双语演讲稿无法完全对应，所以会直接显示为失败，请换一条视频再试。
     </p>
-    <p mb-2>若无法导出请换一条视频再试。</p>
-    <p>
+    <p mb-2>
       2) 若出现1中的情况，会尝试获取台湾版翻译稿，所以你也可能会看到繁体内容。
+    </p>
+    <p mb-2>
+      3)
+      <span text-cyan>「中文背景」</span>和<span text-cyan
+        >「侧边笔记区背景」</span
+      >都可以通过点击变换/取消颜色。
+    </p>
+    <p mb-2>
+      4) 点击<span text-cyan>「作者：xxxx」</span
+      >可以调出打印选项，方便导出为pdf。
     </p>
   </div>
   <div text-center>
