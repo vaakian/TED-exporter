663a04cf37683631e1aae6cfa6425dd61f4811a8
vaakian <vaakian@gmail.com>
2022-05-27 13:43:08 +0800

    `Tools.vue` 2em more from the bottom

--- src/components/Tools.vue
+++ src/components/Tools.vue
@@ -31,7 +31,7 @@ const currentFont = computed(() => {
 <template>
   <div
     id="tools"
-    class="flex left-50% translate-x-[-50%] justify-center bg-blue p-4 gap-2 z-999 fixed bottom-2"
+    class="flex left-50% translate-x-[-50%] justify-center bg-blue p-4 gap-2 z-999 fixed bottom-4"
     border="1 cyan rounded-2"
   >
     <button @click="goHome" class="text-4 p-2">返回</button>
